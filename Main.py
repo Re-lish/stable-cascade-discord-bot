7056b6dd432df20b0c2ef1dc478de459a9f2d00a
T3mplar <56806318+Re-lish@users.noreply.github.com>
2024-03-26 01:43:28 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -26,9 +26,43 @@ def setEmbed(title, description, field1Name="", field1Value=""):
     if field1Name != "" and field1Value != "":
         embed.add_field(name=field1Name, value=field1Value)
     return embed
+    
+def getStyle(prompt):
+    curStyle = ""
+    for i in range(len(prompt)):
+        curStyle += prompt[i]
+        if (prompt[i + 1] == "~" and prompt[i + 2] == "~"):
+            return curStyle
 
+def getUserName(prompt):
+    name = ""
+    index = 0
+    for i in range(len(prompt)):
+        if(prompt[i+1] == ";" and prompt[i+2] == ";"):
+            index = i+3
+            break
+    for i in range(index, len(prompt)):
+        name += prompt[i]
+    return name
+    
 class styleMenu(discord.ui.View):
     @discord.ui.select(placeholder="Pick a style", options=
+    [discord.SelectOption(label="Anime", value="sai-anime"),
+     discord.SelectOption(label="Hyperrealism", value="artstyle-hyperrealism"),
+     discord.SelectOption(label="Cyberpunk", value="game-cyberpunk game"),
+     discord.SelectOption(label="Pixel art", value="sai-pixel art"),
+     discord.SelectOption(label="Low Poly", value="sai-lowpoly"),
+     discord.SelectOption(label="Graffiti", value="artstyle-graffiti"),
+     discord.SelectOption(label="Minecraft", value="game-minecraft"),
+     discord.SelectOption(label="Manga", value="misc-manga"),
+     discord.SelectOption(label="Cinematic", value="sai-cinematic"),
+     discord.SelectOption(label="Origami", value="sai-origami"),
+     discord.SelectOption(label="Kawaii", value="misc-kawaii"),
+     discord.SelectOption(label="Horror", value="misc-horror")
+     ])
+
+    async def select_style(self, interaction: discord.Interaction, select_item: discord.ui.Select):
+   @discord.ui.select(placeholder="Pick a style", options=
     [discord.SelectOption(label="Anime", value="sai-anime"),
      discord.SelectOption(label="Hyperrealism", value="artstyle-hyperrealism"),
      discord.SelectOption(label="Cyberpunk", value="game-cyberpunk game"),
@@ -47,6 +81,7 @@ class styleMenu(discord.ui.View):
         global imageRequestsQueue
         global prompt_
         global IS_GENERATING
+        global QUEUE_IS_EMPTY
         style = select_item.values[0]
         styles = {"sai-anime": "Anime",
                   "artstyle-hyperrealism": "Hyperrealism",
@@ -63,27 +98,43 @@ class styleMenu(discord.ui.View):
                   }
         if (imageRequestsQueue.qsize() == 0):
             QUEUE_IS_EMPTY = True
-        imageRequestsQueue.put(style+"~~"+prompt_)
+        imageRequestsQueue.put(style+"~~"+prompt_+";;"+interaction.user.name)
         await interaction.response.defer()
         orig_msg = await interaction.original_response()
         await orig_msg.edit(content="Your request has been added to the queue.", view=None)
         if(QUEUE_IS_EMPTY == True and IS_GENERATING == False):
             IS_GENERATING = True
             while(imageRequestsQueue.qsize()>=1):
-                promptFromQ = imageRequestsQueue.get()
+                promptFromQ = imageRequestsQueue.queue[0]
                 curStyle = getStyle(promptFromQ)
-                image_Prompt = promptFromQ[len(curStyle)+2:]
+                userName = getUserName(promptFromQ)
+                image_Prompt = promptFromQ[len(curStyle)+2:len(promptFromQ)-len(userName)-2]
                 await interaction.followup.send("Now generating "+ image_Prompt)
                 try:
                     image = await StableDiffusion.generate_image(prompt_=image_Prompt, style_=curStyle)
                     await interaction.followup.send(
-                        content=f"[**ARTSTYLE**: **{styles.get(curStyle)}**] {interaction.user.name} generated: **{image_Prompt}**",
-                        file=discord.File(image))
+                        content=f"[**ARTSTYLE**: **{styles.get(curStyle)}**] {userName} generated: **{image_Prompt}**",
+                        file=discord.File(image), view=upscaleOption())
+                    imageRequestsQueue.get()
                 except:
                     await interaction.followup.send("An error has occurred, most likely the stable diffusion model is not on.")
         IS_GENERATING = False
 
-
+@tree.command(name="viewqueue", description="Shows the current queue",guild=discord.Object(id=serverid))
+async def showQueue(interaction: discord.Interaction):
+    global imageRequestsQueue
+    s = ""
+    if imageRequestsQueue.qsize()==0:
+        await interaction.response.send_message("The queue is empty.")
+    else:
+        for i in range(imageRequestsQueue.qsize()):
+            prompt = imageRequestsQueue.queue[i]
+            userName = getUserName(prompt)
+            style = getStyle(prompt)
+            prompt = prompt[len(style)+2:len(prompt)-len(userName)-2]
+            s+=f"{i+1}. {prompt}. By **{userName}** in **{style}** style\n"
+        await interaction.response.send_message(embed=setEmbed("Current queue", "View the current queue of image generation requests", field1Name="List",field1Value=s))
+        
 @bot.tree.command(name="genimage", description="Generate an image")
 async def func(interaction: discord.Interaction, prompt: str):
     global prompt_
